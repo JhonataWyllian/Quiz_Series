d13953866289e8cff656e2881caa4507373101a5
Livia De Lima Costa <00001096945836sp@aluno.educacao.sp.gov.br>
2026-05-29 11:59:56 -0300

    2commit: adicionei a primeira pergunta

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const perguntas =[
-    
+    {
+    pergunta:"Qual é a série de comedia que acompanha a vida de seis amigos em Nova York?",
+    resposta:["Breaking Bad","Stranger Things", "Friends", "The good doctor"],
+    correta:"Friends"
+    },
+    {
+        
+    }
 ]
